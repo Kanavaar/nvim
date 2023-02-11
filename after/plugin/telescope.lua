471bdbf79ba5ed1b4b3b686713248953a1227d83
kanavaar <uju8765@gmail.com>
2023-02-11 20:13:01 +0100

    adding lsp mappings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fG', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>ht', builtin.colorscheme, {})
