0031d7e2c7ceeb314c08cd51a5d1de729042c0ec
kanavaar <uju8765@gmail.com>
2023-03-04 18:06:16 +0100

    updating lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,10 +3,11 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-  'gopls',
+  -- 'gopls',
   'rust_analyzer',
-  'sumneko_lua',
+  'lua_ls',
   'clangd',
+  'gopls',
 })
 
 local cmp = require('cmp')
